ce9e374e7f084c40dab445c5605493b5b96838f8
GJNorman <113757511+GJNorman@users.noreply.github.com>
2023-02-27 23:17:11 +1100

    Update ExampleApp.c

--- ExampleApp.c
+++ ExampleApp.c
@@ -8,6 +8,7 @@ void processPDOTX1(uint8_t *Data, uint8_t *DLC, uint16_t *NodeIDWithFunctionCode
 int main(void)
 {
   
+ // set up any relevant callbacks - refer to CANMessageDecoder.h -> enum CANCallbackPositions
   SetCANCallback(processPDOTX1,PDO_TX1_CALLBACK);
   
   SetCANCallback(Deal_With_SDO,SDO_TX_CALLBACK);
@@ -15,7 +16,8 @@ int main(void)
   while(true)
   {
     
-    // Read a CAN message and parse into. DATA, Datalength abd nodeID with FunctionCode
+    // Read a CAN message and parse into. DATA, Datalength and nodeID with FunctionCode
+    // you will need to provide the read function yourself
     uint8_t Data[8];
     uint8_t DLC;
     uint16_t NodeIDWithFunctionCode;
