7e57c2b224646c16575ce4627954131e15b48b8e
GJNorman <113757511+GJNorman@users.noreply.github.com>
2023-02-27 23:14:54 +1100

    An example on using Code
    Not intended to be functional

---
+++ ExampleApp.c
@@ -0,0 +1,26 @@
+#include "CANMessageDecoder.h"
+#include "SDO/SDO Server.h
+void processPDOTX1(uint8_t *Data, uint8_t *DLC, uint16_t *NodeIDWithFunctionCode)
+{
+ printf("PDO Message Processed\n"); 
+}
+
+int main(void)
+{
+  
+  SetCANCallback(processPDOTX1,PDO_TX1_CALLBACK);
+  
+  SetCANCallback(Deal_With_SDO,SDO_TX_CALLBACK);
+  
+  while(true)
+  {
+    
+    // Read a CAN message and parse into. DATA, Datalength abd nodeID with FunctionCode
+    uint8_t Data[8];
+    uint8_t DLC;
+    uint16_t NodeIDWithFunctionCode;
+    decodeMessage(&Data, &DLC, &NodeIDWithFunctionCode);
+  }
+ 
+  return 0;
+}
