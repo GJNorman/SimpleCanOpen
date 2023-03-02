b370337f8a5c51bc5b9344f71374a46f6a32af02
GJNorman <113757511+GJNorman@users.noreply.github.com>
2023-03-02 18:38:30 +1100

    Update SDO Client Application.hpp

--- SDO/SDO Client Application.hpp
+++ SDO/SDO Client Application.hpp
@@ -5,9 +5,13 @@
 
 #include <stdint.h>
 #include "SDO Client.h"
-#include "../../utilities/Timer.h"
-#include "../../CAN_Interface.h"
+#include "Timer.h"                  // timout counter
+
 typedef void (*SDO_Complete_Callback_t)(uint8_t *Data, uint8_t len);
+
+typedef void (*CAN_WRITE_CALLBACK_t)(uint8_t Channel, struct *SDO_TX_CAN);
+
+void setSDOAppCANWriteCallback(CAN_WRITE_CALLBACK_t NewCallback);
 
 void SDOApplication(uint8_t Channel, 
                     TimerClass &SDOTimeOutCounter,
@@ -17,4 +21,25 @@ void SDOApplication(uint8_t Channel,
                     uint8_t ServerNode);
 
 
+/*
+example usage
+
+setSDOAppCANWriteCallback(YOUR_CAN_WRITE_FUNCTION);
+
+bool SendInitialSDO = true;
+TimerClass SDOTimeOutCounter(5);                        // 5 second timout
+while(1)
+{
+    // do some stuff
+
+    // process next SDO
+    SDOApplication( Channel, 
+                    SDOTimeOutCounter,
+                    SendInitialSDO, 
+                    OptionalCallbackOnCompletion,
+                    HARDWARE_VERSION,                         //CAN_INDEX
+                    17);                                      // server ID
+    // do some other stuff
+}
+*/
 #endif //SDO_APPLICATION_H
